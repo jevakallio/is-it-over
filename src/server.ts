eb8e4d3bfff108643a071cb62f5ec8a62d350e7d
Jani Eväkallio <jani.evakallio@gmail.com>
2023-10-13 15:34:07 +0100

    Cleanup

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import type * as Party from "partykit/server";
 export default class Server implements Party.Server {
   constructor(readonly party: Party.Party) {}
 
+  // optimistic start :)
   over = false;
 
   onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
@@ -11,9 +12,11 @@ export default class Server implements Party.Server {
 
   onMessage(message: string, sender: Party.Connection) {
     try {
-      const over = JSON.parse(message).over;
-      this.over = over;
-      this.party.broadcast(JSON.stringify({ over, sender: sender.id }));
+      const { over } = JSON.parse(message);
+      if (typeof over === "boolean") {
+        this.over = over;
+        this.party.broadcast(JSON.stringify({ over, sender: sender.id }));
+      }
     } catch (e) {
       console.log(e);
     }
