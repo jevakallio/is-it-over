9281c1df480b640084c7620953a042691dcc46f6
Jani Eväkallio <jani.evakallio@gmail.com>
2023-10-13 14:01:56 +0100

    Basic

--- src/server.ts
+++ src/server.ts
@@ -3,28 +3,20 @@ import type * as Party from "partykit/server";
 export default class Server implements Party.Server {
   constructor(readonly party: Party.Party) {}
 
-  onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
-    // A websocket just connected!
-    console.log(
-      `Connected:
-  id: ${conn.id}
-  room: ${this.party.id}
-  url: ${new URL(ctx.request.url).pathname}`
-    );
+  over = false;
 
-    // let's send a message to the connection
-    conn.send("hello from server");
+  onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
+    conn.send(JSON.stringify({ over: this.over }));
   }
 
   onMessage(message: string, sender: Party.Connection) {
-    // let's log the message
-    console.log(`connection ${sender.id} sent message: ${message}`);
-    // as well as broadcast it to all the other connections in the room...
-    this.party.broadcast(
-      `${sender.id}: ${message}`,
-      // ...except for the connection it came from
-      [sender.id]
-    );
+    try {
+      const over = JSON.parse(message).over;
+      this.over = over;
+      this.party.broadcast(JSON.stringify({ over, sender: sender.id }));
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
 
